20187f92b692f19ffa7474cb734ed2b8c3593bcc
sevinch <nsevinch3@gmail.com>
2024-04-24 11:47:26 +0500

    income migration changed

--- migrations/7_income.up.sql
+++ migrations/7_income.up.sql
@@ -1,20 +1,8 @@
-do $$
-begin
-    if not exists (select * from pg_type where typname = 'status_income_enum') then
-create type status_income_enum as enum (
-        'in_process',
-        'finished',
-        'canceled'
-);
-end if;
-end$$;
-
 create table if not exists incomes (
     id uuid primary key not null,
     branch_id uuid,
     comment varchar(255),
     courier_id uuid ,
-    status status_income_enum,
     created_at timestamp default now() not null,
     updated_at timestamp default now() not null,
     deleted_at timestamp default null,
